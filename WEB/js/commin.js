1fdcfa94289bd379a5027af37b4a7259a6455ef8
wtyxh <287117479@qq.com>
2018-07-18 17:15:15 +0800

    2018-7-18

--- WEB/js/commin.js
+++ WEB/js/commin.js
@@ -1,2 +1,2 @@
 var v_str = $('#mean').html();
-$('#mean').after('<div class="mean mean-fixed">'+v_str+'</div>')
+$('#mean').after('<div class="mean-pardon"><div class="mean mean-fixed">'+v_str+'</div></div>')
